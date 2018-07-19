02e66310fead04e8d3b08110ca213219f0db780e
talor-hammond <talorjhammond@gmail.com>
2018-07-19 16:51:57 +1200

    added logo to Login component

--- client/components/Login.js
+++ client/components/Login.js
@@ -14,7 +14,7 @@ import {
 import LoginForm from './LoginForm'
 
 // Assets:
-// const logo = require('../assets/logo.png')
+const logo = require('../assets/logo.png')
 
 export default class Login extends React.Component { 
     render() {
@@ -22,7 +22,10 @@ export default class Login extends React.Component {
             <KeyboardAvoidingView behavior='padding' style={styles.container}>
                 <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
                     <View style={styles.logoContainer}>
-
+                        <Image
+                            style={styles.logo}
+                            source={logo}
+                        />
                         <Text style={styles.title}>He's just a poor boy, from a poor family</Text>
                     </View>
                 </TouchableWithoutFeedback>
