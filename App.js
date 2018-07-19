0e0130ec39f07e45e9e2ba6b80eae1eadf1f9c66
talor-hammond <talorjhammond@gmail.com>
2018-07-19 18:49:33 +1200

    tabs + navigation working :)

--- App.js
+++ App.js
@@ -3,10 +3,13 @@ import React from 'react';
 // Components:
 import Login from './client/components/signUp/Login.js'
 
+// Testing TabNavigator is working...
+import { Tab } from './client/components/config/navigation'
+
 export default class App extends React.Component {
   render() {
     return (
-      <Login />
+      <Tab />
     )
   }
 }
