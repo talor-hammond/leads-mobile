ff9259fe42e2d05b09643e4923e7a09c10c82961
talor-hammond <talorjhammond@gmail.com>
2018-07-20 19:31:01 +1200

    added working switch navigator; depends on this.state.signedIn

--- App.js
+++ App.js
@@ -5,14 +5,21 @@ import Login from './client/components/signUp/Login.js'
 import Register from './client/components/signUp/Register'
 
 // Testing TabNavigator is working...
-import { MainTabs, LoginStack } from './client/components/config/navigation'
+import { MainTabs, LoginStack, createRootNavigator } from './client/components/config/navigation'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      signedIn: false
+    }
+  }
+
   render() {
-    return (
-      <LoginStack />
-      // <Login />
-      // <Register />
-    )
+    const { signedIn } = this.state // pulling from state...
+    const Layout = createRootNavigator(signedIn)
+
+    return <Layout />
   }
 }
