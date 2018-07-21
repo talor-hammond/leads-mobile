eec3e66d572672c5172c7463d1ca81fab0e31585
talor-hammond <talorjhammond@gmail.com>
2018-07-21 19:30:20 +1200

    added prelim styles

--- client/components/tabs/CommunityMap.js
+++ client/components/tabs/CommunityMap.js
@@ -31,39 +31,53 @@ class CommunityMap extends Component {
         const { region, posts } = this.state // pulling from state...
 
         return (
-                <MapView
-                    style={styles.map}
-                    initialRegion={region}
+            <MapView
+                style={styles.map}
+                initialRegion={region}
+            >
+
+                <MapView.Marker // marker at the user's current location.
+                    coordinate={{
+                        latitude: region.latitude, 
+                        longitude: region.longitude
+                    }}
                 >
+                <View style={styles.radius}>
+                    <View style={styles.marker}></View>
+                </View>
+                </MapView.Marker>
 
                 {
                     posts.map((post, i) => {
                         return (
                             <MapView.Marker
-                            key={i} 
-                            coordinate={{
-                                latitude: post.latitude,
-                                longitude: post.longitude
-                            }}
-                        />
+                                key={i}
+                                coordinate={{
+                                    latitude: post.latitude,
+                                    longitude: post.longitude
+                                }}
+                            />
                         )
                     })
                 }
 
-                </MapView>
+            </MapView>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'blue',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
     map: {
         flex: 1
+    },
+    marker: {
+        height: 20,
+        width: 20,
+        borderWidth: 3,
+        borderColor: 'white',
+        borderRadius: 20 / 2,
+        overflow: 'hidden',
+        backgroundColor: '#007AFF'
     }
 })
 
