7188640952cf92caa908b5c1c5a2c43d3e97c11d
talor-hammond <talorjhammond@gmail.com>
2018-07-22 14:24:33 +1200

    added custom dark map style to mapview

--- client/components/tabs/CommunityMap.js
+++ client/components/tabs/CommunityMap.js
@@ -21,8 +21,8 @@ class CommunityMap extends Component {
             posts: [ // feed in our redux 'posts' state here!
                 {
                     id: 1,
+                    topic: 'Car troubles',
                     title: 'My car broke down foo',
-                    category: 'Car troubles',
                     description: 'Can someone bring me jumper leads plz!',
                     latitude: 37.78825,
                     longitude: -122.2324, // WE NEED SOME CUBA ST DATA
@@ -38,6 +38,8 @@ class CommunityMap extends Component {
         return (
             <MapView
                 style={styles.map}
+                provider={MapView.PROVIDER_GOOGLE}
+                customMapStyle={generatedMapStyle}
                 initialRegion={region}
             >
 
@@ -48,10 +50,10 @@ class CommunityMap extends Component {
                     }}
                     title='Hi there!'
                     description='This is your current location :~)'
-                > 
-                    <View style={[styles.marker, styles.markerWrap]}> {/* Styling a particular marker; overrides the default */}
-                        <View style={styles.ring}></View>
-                    </View>
+                >
+                    <Animated.View style={[styles.marker, styles.markerWrap]}> {/* Styling a particular marker; overrides the default */}
+                        <Animated.View style={styles.ring}></Animated.View>
+                    </Animated.View>
                 </MapView.Marker>
 
                 {
@@ -66,9 +68,9 @@ class CommunityMap extends Component {
                                 title={post.title}
                                 description={post.description}
                             >
-                            <View style={[styles.otherMarker, styles.markerWrap]}>
-                                <View style={styles.otherRing}></View>
-                            </View>
+                                <View style={[styles.otherMarker, styles.markerWrap]}>
+                                    <View style={styles.otherRing}></View>
+                                </View>
                             </MapView.Marker>
                         )
                     })
@@ -118,5 +120,220 @@ const styles = StyleSheet.create({
         borderColor: "rgba(130,4,0, 0.5)"
     }
 })
+
+// Our custom map style:
+const generatedMapStyle = [
+    {
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#242f3e"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#746855"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#242f3e"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.locality",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#d59563"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#d59563"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.business",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#263c3f"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#6b9a76"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#38414e"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#212a37"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9ca5b3"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#746855"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#1f2835"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#f3d19c"
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#2f3948"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.station",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#d59563"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#17263c"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#515c6d"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#17263c"
+        }
+      ]
+    }
+  ]
 
 export default CommunityMap
