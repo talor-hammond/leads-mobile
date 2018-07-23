fe2bb2939f12caf0356f5251b95eb46f5a3a0029
talor-hammond <talorjhammond@gmail.com>
2018-07-23 19:57:39 +1200

    edited local storage

--- client/utils/localstorage.js
+++ client/utils/localstorage.js
@@ -20,19 +20,19 @@ import { AsyncStorage } from 'react-native'
 // }
 
 export async function get (key) {
-  // return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJ0YXl0YXkiLCJpYXQiOjE1MzIyOTg3NDYsImV4cCI6MTUzMjM4NTE0Nn0.h2ofNzffbeuugnIILSSRLX4Y-VyXAJvIfhkP4DgCQ7E"
+  return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJ0YXl0YXkiLCJpYXQiOjE1MzIyOTg3NDYsImV4cCI6MTUzMjM4NTE0Nn0.h2ofNzffbeuugnIILSSRLX4Y-VyXAJvIfhkP4DgCQ7E"
 
-  try {
-    const value = await AsyncStorage.getItem(key);
-    return value
+  // try {
+  //   const value = await AsyncStorage.getItem(key);
+  //   return value
 
-    // if (value != null) {
-      // console.log(value)
-    // }
-  } catch (error) {
-    //Error retrieving data
-    return null
-  }
+  //   // if (value != null) {
+  //     // console.log(value)
+  //   // }
+  // } catch (error) {
+  //   //Error retrieving data
+  //   return null
+  // }
 }
 
 export async function set (key, value) {
