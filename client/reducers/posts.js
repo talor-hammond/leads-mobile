a25670d5577ef1b05304973e5161800e5c58d4dd
rikiparata <rikiipee47@gmail.com>
2018-07-23 14:56:17 +1200

    added two more post reducers

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,4 +1,4 @@
-import { GET_POSTS, ADD_POST } from '../actions/posts'
+import { GET_POSTS, ADD_POST, GET_POST_BY_POST_ID, GET_POSTS_BY_USER_ID } from '../actions/posts'
 
 export default function posts(state = [], action) {
     let newState = [...state]
@@ -8,6 +8,10 @@ export default function posts(state = [], action) {
             return [...action.posts]
         case ADD_POST:
             return [...newState, action.post] // spread everything out from our current state, as well as our added post into an array   
+        case GET_POST_BY_POST_ID:
+            return [...action.post]
+        case GET_POSTS_BY_USER_ID:
+            return [...action.posts]
         default:
             return state
     }
