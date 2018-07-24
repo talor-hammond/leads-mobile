d200ecdcab116f4812b5f7426f94c4161035f332
talor-hammond <talorjhammond@gmail.com>
2018-07-24 17:19:05 +1200

    comments sectiongit add .git add .git add .

--- client/components/tabs/homeComponents/Comment.js
+++ client/components/tabs/homeComponents/Comment.js
@@ -13,7 +13,37 @@ import { Card, CardItem, Thumbnail, Body, Left, Right, Button, Icon } from 'nati
 class Comment extends Component {
     render() {
         return(
-            
+            <View style={styles.container}>
+                <View style={styles.commentText}>
+                    <Text>Hi</Text>
+                    <Text style={styles.datePublished}>24 July 2018</Text>
+                </View>
+                <View style={styles.commentUser}>
+                    <Thumbnail source={require('../../../assets/user.png')} /> 
+                </View>
+            </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        margin: 2.5,
+    },
+    commentText: {
+        flex: 3,
+        marginLeft: 10
+    },
+    commentUser: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    datePublished: {
+        fontStyle: 'italic',
+        color: 'rgba(140, 140, 140, 0.8)'
+    }
+})
+
+export default Comment
--- client/components/tabs/homeComponents/FullPost.js
+++ client/components/tabs/homeComponents/FullPost.js
@@ -10,6 +10,8 @@ import {
     ScrollView
 } from 'react-native'
 
+import Comment from './Comment'
+
 import { Card, CardItem, Thumbnail, Body, Left, Right, Button, Icon } from 'native-base'
 
 class FullPost extends Component {
@@ -22,31 +24,59 @@ class FullPost extends Component {
         console.log(this.props)
         return (
                 <View style={styles.container}>
+                    <View style={styles.postContainer}>
                     <View style={styles.header}>
                         {/* OP's user avatar */}
                         <Text style={styles.postTitle}>POST TITLE HERE</Text>
                     </View>
+
                     <View style={styles.postContent}>
-                        <Text>George Bush doesn't care about black people. My greatest 
-                            pain in life is that I will never be able to see myself perform live.
-                            Nothing in life is promised except death.
-                        </Text>                        
-                        <View style={styles.buttonContainer}>
-                            <Text>Add Comment</Text> {/* Add comment button here */}
-                            <Text>Go to maps</Text> {/* Go to Maps button here */}
+                        <View style={styles.descriptionContainer}>
+                            <Text>George Bush doesn't care about black people. My greatest 
+                                pain in life is that I will never be able to see myself perform live.
+                                Nothing in life is promised except death.
+                            </Text>                        
+                        </View>
+
+                        <View style={styles.addressContainer}>
+                            <Text style={styles.address}>123 Fake Street</Text> {/* make this open gps? */}
                         </View>
                     </View>
-                    <View style={styles.postComments}>
-                        <ScrollView>
-                            <CardItem />
-                            <CardItem />
-                            <CardItem />
-                        </ScrollView>
-                    </View>
-                    <View style={styles.backButtonContainer}>
-                        <TouchableOpacity onPress={() => this.props.togglePostModal()} style={styles.button}>
-                            <Text style={styles.backButtonText}>Back</Text>
-                        </TouchableOpacity>
+
+                        <View style={styles.postComments}>
+                            <View style={styles.commentsTitleContainer}>
+                                <Text style={styles.commentsTitle}>Comments</Text>
+                            </View>
+
+                            <ScrollView>
+                                <Comment />
+                                <Comment />
+                                <Comment />
+                                <Comment />
+                                <Comment />
+                            </ScrollView>
+                            <View style={styles.commentContainer}>
+                                <TextInput
+                                    placeholder="Write a comment..."
+                                    placeholderTextColor='rgba(140, 140, 140, 0.8)'
+                                    autoCapitalize="none"
+                                    autoCorrect={true}
+                                    style={styles.input}
+                                />
+                                <View style={styles.commentButton}>
+                                    <TouchableOpacity onPress={() => this.addComment()}>
+                                        <Text style={styles.buttonText}>Add</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </View>
+
+                        <View style={styles.backButtonContainer}>
+                            <TouchableOpacity onPress={() => this.props.togglePostModal()} style={styles.button}>
+                                <Text style={styles.backButtonText}>Back</Text>
+                            </TouchableOpacity>
+                        </View>
+
                     </View>
                 </View>
         )
@@ -56,11 +86,51 @@ class FullPost extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#EAEAEA',
-        justifyContent: 'space-around'
+        backgroundColor: 'white',
+    },
+    postContainer: {
+        flex: 1,
+        marginTop: 35,
+        backgroundColor: '#EAEAEA'
     },
     header: {
-        marginHorizontal: 10
+        marginHorizontal: 10,
+        marginVertical: 20
+    },
+    postTitle: {
+        fontWeight: '700',
+        fontSize: 20
+    },
+    descriptionContainer: {
+        marginHorizontal: 30
+    },
+    addressContainer: {
+        marginTop: 10,
+        marginHorizontal: 30
+    },
+    address: {
+        fontStyle: 'italic'
+    },
+    postComments: {
+        marginTop: 20,
+        marginHorizontal: 20,
+        backgroundColor: 'white',
+        borderRadius: 7
+
+    },
+    commentsTitleContainer: {
+        alignItems: 'center',
+        marginVertical: 15
+    },
+    commentContainer: {
+        marginVertical: 10,
+        marginHorizontal: 8,
+        flexDirection: 'row',
+        
+    },
+    commentsTitle: {
+        fontWeight: '700',
+        fontSize: 16
     },
     backButtonContainer: {
         marginTop: 30,
@@ -70,10 +140,18 @@ const styles = StyleSheet.create({
         fontWeight: '700',
         textAlign: 'center'
     },
-    buttonContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-around'
-    }
+    input: {
+        flex: 3
+    },
+    commentButton: {
+        flex: 1,
+        backgroundColor: 'grey',
+        borderRadius: 4
+    },
+    buttonText: {
+        textAlign: 'center'
+    },
+
 })
 
 export default FullPost
